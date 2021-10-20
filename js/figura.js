9182920e7abdb69171f359e5d03e073a82a67cc8
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2021-10-20 18:19:56 +0300

    Task solution

--- js/figura.js
+++ js/figura.js
@@ -1,8 +1,19 @@
 class Figura {
     static randomFiguros(amount) {
+        const result = [];
+        for (let i = 0; i < amount; i++) {
+            result[i] = {};
+
+            result[i].width = Math.round(Math.random()*9) + 1;
+            result[i].height = Math.round(Math.random()*9) + 1;
+
+            console.log(result[i]);
+        }
+        return result;
     }
 
     static size(width, height) {
+        return width * height;
     }
 }
 
